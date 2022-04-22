31322fa9e064137a8ab3004b84aa2f92de7c8039
cesarmg16 <cesarmoro.daw@ciudadescolarfp.es>
2022-04-22 13:06:15 +0200

    commit01

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author César Moro García
+ * @version 2022.04.22
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Listado de alumnos 1º DAW");
+	}
+
+}
