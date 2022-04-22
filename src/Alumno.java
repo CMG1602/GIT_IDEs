700ef57133d77aac63e96d2325e9538f3b523661
cesarmg16 <cesarmoro.daw@ciudadescolarfp.es>
2022-04-22 13:10:36 +0200

    commit01_cesar

---
+++ src/Alumno.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @author César Moro García
+ * @version 2022.04.22
+ *
+ */
+public class Alumno {
+	private String nombre;
+	private String apellido;
+	private String dni;
+}
